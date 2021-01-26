8180be8cdfda0ac543ddbd309340ce054bf65883
sandeepdhungana <sandeepdhungana10@gmail.com>
2021-01-26 11:57:09 +0530

    frontend first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCJxmoAUWCwRcrqn8TFj8RgYNCP9iHSeTo",
+  authDomain: "whatsapp-clone-c1e4d.firebaseapp.com",
+  projectId: "whatsapp-clone-c1e4d",
+  storageBucket: "whatsapp-clone-c1e4d.appspot.com",
+  messagingSenderId: "75795949937",
+  appId: "1:75795949937:web:7e89ac49a666551de12ed0",
+};
